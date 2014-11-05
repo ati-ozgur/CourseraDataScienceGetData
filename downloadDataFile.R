b8678632aa46a451eedb9a84c085ae67d6c92fe5
Atilla Ozgur <ati.ozgur@gmail.com>
2014-11-06 01:02:54 +0200

    filename correction

--- downloadDataFile.R
+++ downloadDataFile.R
@@ -1,7 +1,7 @@
 zipFileName = "data.zip"
 
 
-if (!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+if (!file.exists(zipFileName)) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl, destfile = zipFileName, method = "curl")  
   
