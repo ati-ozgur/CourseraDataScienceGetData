bd24cffe0fd140aee6991cc52bfc3b10b5a896c6
Atilla Ozgur <ati.ozgur@gmail.com>
2014-11-13 00:26:04 +0200

    read train data and change colNames in it

--- CreateOneDataSetFromTrainAndTestSet.R
+++ CreateOneDataSetFromTrainAndTestSet.R
@@ -1,6 +1,23 @@
-CreateOneDataSetFromTrainAndTestSet.R
-
+library("stringr")
 # Merges the training and the test sets to create one data set.
 
 
-X_train <- read.table("UCI HAR Dataset/train/X_train.txt", quote="\"")
+trainFeatures <- read.table("UCI HAR Dataset/train/X_train.txt", quote="\"")
+trainClass <- read.table("UCI HAR Dataset/train/Y_train.txt", quote="\"")
+
+
+features <- read.table("UCI HAR Dataset/features.txt", quote="\"")
+colnames(features) <- c("ID","FeatureName")
+
+features[,2] <- str_replace_all(features[,2],"\\(\\)","")
+
+features[,2] <- str_replace_all(features[,2],"\\(","_")
+features[,2] <- str_replace_all(features[,2],"\\)","_")
+
+features[,2] <- str_replace_all(features[,2],",","_")
+
+colnames(trainFeatures) <- features[,2] 
+
+
+#names(features)[1] <- "ID"
+#names(features)[2] <- "FeatureName"
