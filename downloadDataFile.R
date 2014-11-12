c101750a87d99a05d66ae3fdd1c816da9449f446
Atilla Ozgur <ati.ozgur@gmail.com>
2014-11-12 23:35:53 +0200

    control if data set directory exists

--- downloadDataFile.R
+++ downloadDataFile.R
@@ -1,9 +1,14 @@
 zipFileName = "data.zip"
+dataSetDirectoryName = "UCI HAR Dataset"
 
 
-if (!file.exists(zipFileName)) {
-  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileUrl, destfile = zipFileName, method = "curl")  
-  
+if (!file.exists(dataSetDirectoryName)){
+  if (!file.exists(zipFileName)) {
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(fileUrl, destfile = zipFileName, method = "curl")  
+    
+  }
+  unzip(zipFileName)  
 }
-unzip(zipFileName)
+
+
