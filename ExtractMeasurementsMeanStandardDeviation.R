51c55928f5bd60c0906d567f92bad16904cf52e8
Atilla Ozgur <ati.ozgur@gmail.com>
2014-11-23 23:44:27 +0200

    refactor

--- ExtractMeasurementsMeanStandardDeviation.R
+++
@@ -1,7 +0,0 @@
-wantedFeatures <- unique( which(!is.na(str_match(names(mergedDataset),"Mean|mean|Std|std|activityLabel"))))
-
-
-mergedDataset_extracted <- mergedDataset[,wantedFeatures]
-
-
-
--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,10 @@ source("CreateOneDataSetFromTrainAndTestSet.R")
 
 
 #2.Extracts only the measurements on the mean and standard deviation for each measurement.
-source("ExtractMeasurementsMeanStandardDeviation.R")
+
+wantedFeatures <- unique( which(!is.na(str_match(names(mergedDataset),"Mean|mean|Std|std|activityLabel"))))
+mergedDataset_extracted <- mergedDataset[,wantedFeatures]
+
 
 
 #3.Uses descriptive activity names to name the activities in the data set
@@ -24,6 +27,8 @@ mergedDataset_extracted[mergedDataset_extracted$activityLabel == 6,87] <- "LAYIN
 
 
 #4.Appropriately labels the data set with descriptive variable names. 
+# Features renamed before. 
+
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
