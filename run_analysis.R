ae3a9df0f3ca02855b12d4a0aae8b28aaa6bed95
Atilla Ozgur <ati.ozgur@gmail.com>
2014-11-23 23:42:35 +0200

    Uses descriptive activity names to name the activities in the data set DONE

--- run_analysis.R
+++ run_analysis.R
@@ -11,6 +11,18 @@ source("ExtractMeasurementsMeanStandardDeviation.R")
 
 
 #3.Uses descriptive activity names to name the activities in the data set
+
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 1,87] <- "WALKING"
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 2,87] <- "WALKING_UPSTAIRS"
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 3,87] <- "WALKING_DOWNSTAIRS"
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 4,87] <- "SITTING"
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 5,87] <- "STANDING"
+mergedDataset_extracted[mergedDataset_extracted$activityLabel == 6,87] <- "LAYING"
+
+
+
+
+
 #4.Appropriately labels the data set with descriptive variable names. 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
